f8f4cdd407f37b991fce772158b22e6ca650da30
jms-50 <jcchkwjd@naver.com>
2025-11-05 14:36:27 +0900

    과제 제출

--- duck.cpp
+++ duck.cpp
@@ -1,22 +1,36 @@
 #include "duck.h"
 
 /* TODO */
-void Duck::performFly(){}
+void Duck::performFly(){
+    this->flyBehavior->fly();
+}
 /* TODO */
-void Duck::performQuack(){}
+void Duck::performQuack(){
+    this->quackBehavior->quack();
+}
 /* TODO */
-void Duck::setFlyBehavior(FlyBehavior* fb){}
+void Duck::setFlyBehavior(FlyBehavior* fb){
+    if(this->flyBehavior){
+        delete this->flyBehavior;
+    }
+    this->flyBehavior = fb;
+}
 /* TODO */
-void Duck::setQuackBehavior(QuackBehavior* qb){}
+void Duck::setQuackBehavior(QuackBehavior* qb){
+    if(this->quackBehavior){
+        delete this->quackBehavior;
+    }
+    this->quackBehavior = qb;    
+}
 
 
 /* TODO */
-MallardDuck::MallardDuck() {}
+MallardDuck::MallardDuck() : Duck(new FlyWithWings,new Quack){}
 /* TODO */
-RedheadDuck::RedheadDuck() {}
+RedheadDuck::RedheadDuck() : Duck(new FlyWithWings,new Quack) {}
 /* TODO */
-RubberDuck::RubberDuck() {}
+RubberDuck::RubberDuck() : Duck(new FlyNoWay, new Squeak){}
 /* TODO */
-DecoyDuck::DecoyDuck() {}
+DecoyDuck::DecoyDuck() : Duck(new FlyNoWay, new MuteQuack){}
 /* TODO */
-ModelDuck::ModelDuck() {}
+ModelDuck::ModelDuck() : Duck(new FlyNoWay, new MuteQuack){}
